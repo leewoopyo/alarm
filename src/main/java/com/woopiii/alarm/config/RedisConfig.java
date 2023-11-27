940b06130cec779a40c03ee1f64180152f4692c1
Lstamp <lozenkrantz@gmail.com>
2023-11-28 07:03:06 +0900

    [ADD] settings

---
+++ src/main/java/com/woopiii/alarm/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.woopiii.alarm.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private Integer port;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+
+        System.out.println(host);
+        System.out.println(port);
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
+}
---
+++ src/main/resources/initdata/h2db/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO sample (id, name) values (1, 'sample1');
+INSERT INTO sample (id, name) values (2, 'sample2');
+INSERT INTO sample (id, name) values (3, 'sample3');
+INSERT INTO sample (id, name) values (4, 'sample4');
+INSERT INTO sample (id, name) values (5, 'sample5');
